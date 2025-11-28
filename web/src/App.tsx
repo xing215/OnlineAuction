439be9abf71a461072402026e39e4acd969776ec
nhphung <nhphuung@gmail.com>
2025-11-28 10:27:54 +0700

    update css product card

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,11 +2,12 @@ import "./App.css";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import { WEB_PAGE } from "./constants/webPages";
-import ProductListPage from "./pages/ProductListPage";
+// import ProductListPage from "./pages/ProductListPage";
 import MyProductsPage from "./pages/MyProductsPage";
 import SignInPage from "./pages/Signin";
 import SignUpPage from "./pages/Signup";
 import Profile from "./pages/Profile";
+import HomePage from "./pages/HomePage/HomePage";
 
 interface PlaceholderPageProps {
   title: string;
@@ -30,7 +31,7 @@ function App() {
       <div className="app-container">
         <Routes>
           <Route element={<Layout />}>
-            <Route path={WEB_PAGE.HOME.path} element={<ProductListPage />} />
+            <Route path={WEB_PAGE.HOME.path} element={<HomePage />} />
             <Route path={WEB_PAGE.CATEGORIES.path} element={<PlaceholderPage title={WEB_PAGE.CATEGORIES.label} />} />
             <Route path={WEB_PAGE.FAVORITES.path} element={<PlaceholderPage title={WEB_PAGE.FAVORITES.label} />} />
             <Route path={WEB_PAGE.BID_HISTORY.path} element={<PlaceholderPage title={WEB_PAGE.BID_HISTORY.label} />} />
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,23 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import "./index.css";
-import App from "./App.tsx";
-import HomePage from "./pages/HomePage/HomePage.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      { path: "", element: <HomePage /> },
-    ],
-  },
-]);
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>
 );
