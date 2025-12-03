086d75970c81901a3a6375025fbb15400b19ea99
nhphung <nhphuung@gmail.com>
2025-12-03 16:04:54 +0700

    update: format date

--- web/src/utilities/FormatDate.ts
+++ web/src/utilities/FormatDate.ts
@@ -1,11 +1,11 @@
-export const formatDate = (isoString: string | undefined | null): string => {
+export const formatDate = (isoString: string | undefined | null | Date): string => {
   if (!isoString) return "";
   
   const date = new Date(isoString);
   
   // Kiểm tra nếu date không hợp lệ
-  if (isNaN(date.getTime())) return isoString || "";
-
+  if (isNaN(date.getTime())) return isoString instanceof Date ? isoString.toString() : isoString || "";
+  
   const day = String(date.getDate()).padStart(2, '0');
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const year = date.getFullYear();
